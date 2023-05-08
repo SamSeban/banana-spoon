f998e05c9d442de8e582ec18d9f6c373612e9a61
kpu6neco <132841004+kpu6neco@users.noreply.github.com>
2023-05-08 13:04:28 +0300

    Update lang.js

--- js/lang.js
+++ js/lang.js
@@ -2,14 +2,14 @@ var langStrings = {
     en: {
         new_student_btn_text: "I'm a new student",
         started_degree_btn_text: "I already started my degree",
-        courses_input_placeholder: "Copy and paste your courses here",
+        courses_input_placeholder: "On students website Cmd+A the page of courses in the My Grades page here",
         lang_switcher_img_src: "img/il.png",
         submit_btn_text: "Submit"
     },
     he: {
         new_student_btn_text: "אני סטודנט חדש",
         started_degree_btn_text: "כבר התחלתי את התואר",
-        courses_input_placeholder: "העתק והדבק את הקורסים שלך כאן",
+        courses_input_placeholder: "באתר הסטודנטים העתק והדבק עמוד הקורסים בעמוד הציונים שלי כאן",
         lang_switcher_img_src: "img/uk.png",
         submit_btn_text: "שלח"
     }
