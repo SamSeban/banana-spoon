f37a7b4e6b25dd4430ddeb55ec3639795e85e804
Sam Seban <samseban@icloud.com>
2023-05-08 15:44:18 +0300

    Clearer instructions

--- js/lang.js
+++ js/lang.js
@@ -2,15 +2,21 @@ var langStrings = {
     en: {
         new_student_btn_text: "I'm a new student",
         started_degree_btn_text: "I already started my degree",
-        courses_input_placeholder: "On students website Cmd+A the page of courses in the My Grades page here",
+        courses_input_placeholder: "Paste the content here.",
         lang_switcher_img_src: "img/il.png",
-        submit_btn_text: "Submit"
+        submit_btn_text: "Submit",
+        text_direction: "ltr",
+        text_align: "left",
+        instructions: "Go on the <a href=\"https://students.technion.ac.il/local/tcurricular/grades\" target=\"_blank\">students website</a>, select the entire page using CTRL+A and paste it in the textbox below."
     },
     he: {
         new_student_btn_text: "אני סטודנט חדש",
         started_degree_btn_text: "כבר התחלתי את התואר",
-        courses_input_placeholder: "באתר הסטודנטים העתק והדבק עמוד הקורסים בעמוד הציונים שלי כאן",
+        courses_input_placeholder: "הדבק את התוכן כאן.",
         lang_switcher_img_src: "img/uk.png",
-        submit_btn_text: "שלח"
+        submit_btn_text: "שלח",
+        text_direction: "rtl",
+        text_align: "right",
+        instructions: "כנסו לאתר <a href=\"https://students.technion.ac.il/local/tcurricular/grades\" target=\"_blank\">students</a>, בחרו את כל העמוד באמצעות CTRL+A והדביקו אותו בתיבת הטקסט למטה."
     }
 };
